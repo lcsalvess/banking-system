0bbd5b762ee1854218e25dfdfbebc88f43052a5d
Lucas Alves <lucas0asm@outlook.com>
2026-09-09 18:11:44 -0300

    test: add withdrawal service tests

--- src/test/java/com/lucas/sistemabancario/service/TransacaoServiceTest.java
+++ src/test/java/com/lucas/sistemabancario/service/TransacaoServiceTest.java
@@ -7,6 +7,7 @@ import com.lucas.sistemabancario.entity.enums.SituacaoConta;
 import com.lucas.sistemabancario.entity.enums.TipoTransacao;
 import com.lucas.sistemabancario.exception.ContaIsNotActiveException;
 import com.lucas.sistemabancario.exception.RendimentoJaAplicadoException;
+import com.lucas.sistemabancario.exception.SaldoIsNotEnoughException;
 import com.lucas.sistemabancario.exception.ValorInvalidoException;
 import com.lucas.sistemabancario.repository.TransacaoRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -83,6 +84,72 @@ public class TransacaoServiceTest {
             Transacao transacao = transacaoCaptor.getValue();
             assertEquals(TipoTransacao.DEPOSITO, transacao.getTipoTransacao());
             assertEquals(BigDecimal.TEN, transacao.getValor());
+            assertEquals(conta, transacao.getConta());
+        }
+    }
+
+    @Nested
+    @DisplayName("Testes de saque")
+    class SaqueTests {
+        private final Long contaId = 1L;
+        private Conta conta;
+
+        @BeforeEach
+        void setUp() {
+            conta = new Conta() {
+            };
+            ReflectionTestUtils.setField(conta, "id", contaId);
+            ReflectionTestUtils.setField(conta, "saldo", new BigDecimal("100.00"));
+            when(contaService.buscarContaPorId(contaId)).thenReturn(conta);
+        }
+
+        @Test
+        @DisplayName("Deve lançar exceção ao tentar sacar de uma conta CANCELADA.")
+        void deveLancarExcecaoQuandoContaNaoEstaAtiva() {
+            ReflectionTestUtils.setField(conta, "situacaoConta", SituacaoConta.CANCELADA);
+            assertThrows(ContaIsNotActiveException.class, () -> transacaoService.sacar(contaId, BigDecimal.TEN));
+            verify(transacaoRepository, never()).save(any());
+        }
+
+        @ParameterizedTest
+        @DisplayName("Deve lançar exceção ao tentar sacar com valores inválidos (null, zero ou negativo).")
+        @NullSource
+        @ValueSource(strings = {"0.00", "-0.01", "-10.00"})
+        void deveLancarExcecaoQuandoValorEhInvalido(BigDecimal valorInvalido) {
+            assertThrows(ValorInvalidoException.class, () -> transacaoService.sacar(contaId, valorInvalido));
+            verify(transacaoRepository, never()).save(any());
+        }
+
+        @Test
+        @DisplayName("Deve lançar exceção ao tentar sacar mais do que tem em conta.")
+        void deveLancarExcecaoQuandoSaqueEhMaiorQueSaldo() {
+            assertThrows(SaldoIsNotEnoughException.class, () -> transacaoService.sacar(contaId, new BigDecimal("200.00")));
+            verify(transacaoRepository, never()).save(any());
+        }
+
+        @Test
+        @DisplayName("Deve retornar sucesso ao tentar sacar o mesmo valor do saldo.")
+        void deveRealizarSaqueQuandoValorSaqueIgualASaldo() {
+            ArgumentCaptor<Transacao> transacaoCaptor = ArgumentCaptor.forClass(Transacao.class);
+            transacaoService.sacar(contaId, new BigDecimal("100.00"));
+            assertEquals(new BigDecimal("0.00"), conta.getSaldo());
+            verify(transacaoRepository).save(transacaoCaptor.capture());
+            Transacao transacao = transacaoCaptor.getValue();
+            assertEquals(TipoTransacao.SAQUE, transacao.getTipoTransacao());
+            assertEquals(new BigDecimal("100.00"), transacao.getValor());
+            assertEquals(conta, transacao.getConta());
+        }
+
+        @Test
+        @DisplayName("Deve retornar sucesso ao tentar sacar valor menor que o saldo.")
+        void deveRealizarSaqueQuandoValorMenorQueSaldo() {
+            ArgumentCaptor<Transacao> transacaoCaptor = ArgumentCaptor.forClass(Transacao.class);
+            transacaoService.sacar(contaId, new BigDecimal("30.00"));
+            assertEquals(new BigDecimal("70.00"), conta.getSaldo());
+            verify(transacaoRepository).save(transacaoCaptor.capture());
+            Transacao transacao = transacaoCaptor.getValue();
+            assertEquals(TipoTransacao.SAQUE, transacao.getTipoTransacao());
+            assertEquals(new BigDecimal("30.00"), transacao.getValor());
             assertEquals(conta, transacao.getConta());
         }
     }

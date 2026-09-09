ed2bbd33ccb2cf2bf07e2b2e928ee4dc788670d4
Lucas Alves <lucas0asm@outlook.com>
2026-09-09 17:04:32 -0300

    refactor: improve deposit unit tests structure

--- src/test/java/com/lucas/sistemabancario/service/TransacaoServiceTest.java
+++ src/test/java/com/lucas/sistemabancario/service/TransacaoServiceTest.java
@@ -8,13 +8,18 @@ import com.lucas.sistemabancario.exception.ContaIsNotActiveException;
 import com.lucas.sistemabancario.exception.RendimentoJaAplicadoException;
 import com.lucas.sistemabancario.exception.ValorInvalidoException;
 import com.lucas.sistemabancario.repository.TransacaoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
 
@@ -35,15 +40,21 @@ public class TransacaoServiceTest {
     @Nested
     @DisplayName("Testes de depósito")
     class DepositoTests {
+        private final Long contaId = 1L;
+        private Conta conta;
+
+        @BeforeEach
+        void setUp() {
+            conta = new Conta() {
+            };
+            ReflectionTestUtils.setField(conta, "id", contaId);
+        }
+
         @Test
         @DisplayName("Deve lançar exceção ao tentar depositar em uma conta CANCELADA.")
         void deveLancarExcecaoQuandoContaNaoEstaAtiva() {
-            Long contaId = 1L;
-            Conta conta = mock(Conta.class);
-            when(conta.getSituacaoConta())
-                    .thenReturn(SituacaoConta.CANCELADA);
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(conta);
+            ReflectionTestUtils.setField(conta, "situacaoConta", SituacaoConta.CANCELADA);
+            when(contaService.buscarContaPorId(contaId)).thenReturn(conta);
             assertThrows(ContaIsNotActiveException.class, () -> transacaoService.depositar(contaId, BigDecimal.TEN));
             verify(transacaoRepository, never()).save(any());
         }
@@ -51,52 +62,18 @@ public class TransacaoServiceTest {
         @Test
         @DisplayName("Deve realizar depósito em uma conta ATIVA e valor VÁLIDO.")
         void deveRealizarDepositoQuandoContaEstaAtivaEValorValido() {
-            Long contaId = 1L;
-            Conta conta = mock(Conta.class);
-            when(conta.getSituacaoConta())
-                    .thenReturn(SituacaoConta.ATIVA);
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(conta);
+            when(contaService.buscarContaPorId(contaId)).thenReturn(conta);
             transacaoService.depositar(contaId, BigDecimal.TEN);
             verify(transacaoRepository).save(any(Transacao.class));
         }
 
-        @Test
-        @DisplayName("Deve lançar exceção ao tentar depositar com valor null.")
-        void deveLancarExcecaoQuandoValorEhNull() {
-            Long contaId = 1L;
-            Conta conta = mock(Conta.class);
-            when(conta.getSituacaoConta())
-                    .thenReturn(SituacaoConta.ATIVA);
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(conta);
-            assertThrows(ValorInvalidoException.class, () -> transacaoService.depositar(contaId, null));
-            verify(transacaoRepository, never()).save(any());
-        }
-
-        @Test
-        @DisplayName("Deve lançar exceção ao tentar depositar com valor zero.")
-        void deveLancarExcecaoQuandoValorEhZero() {
-            Long contaId = 1L;
-            Conta conta = mock(Conta.class);
-            when(conta.getSituacaoConta())
-                    .thenReturn(SituacaoConta.ATIVA);
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(conta);
-            assertThrows(ValorInvalidoException.class, () -> transacaoService.depositar(contaId, BigDecimal.ZERO));
-            verify(transacaoRepository, never()).save(any());
-        }
-
-        @Test
-        @DisplayName("Deve lançar exceção ao tentar depositar com valor negativo.")
-        void deveLancarExcecaoQuandoValorEhNegativo() {
-            Long contaId = 1L;
-            Conta conta = mock(Conta.class);
-            when(conta.getSituacaoConta())
-                    .thenReturn(SituacaoConta.ATIVA);
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(conta);
-            assertThrows(ValorInvalidoException.class, () -> transacaoService.depositar(contaId, new BigDecimal("-10.00")));
+        @ParameterizedTest
+        @DisplayName("Deve lançar exceção ao tentar depositar com valores inválidos (null, zero ou negativo).")
+        @NullSource
+        @ValueSource(strings = {"0.00", "-0.01", "-10.00"})
+        void deveLancarExcecaoQuandoValorEhInvalido(BigDecimal valorInvalido) {
+            when(contaService.buscarContaPorId(contaId)).thenReturn(conta);
+            assertThrows(ValorInvalidoException.class, () -> transacaoService.depositar(contaId, valorInvalido));
             verify(transacaoRepository, never()).save(any());
         }
     }
@@ -109,14 +86,10 @@ public class TransacaoServiceTest {
         void deveLancarExcecaoQuandoRendimentoJaAplicadoNoDia() {
             Long contaId = 1L;
             ContaPoupanca contaPoupanca = mock(ContaPoupanca.class);
-            when(contaPoupanca.getId())
-                    .thenReturn(contaId);
-            when(contaPoupanca.getSituacaoConta())
-                    .thenReturn(SituacaoConta.ATIVA);
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(contaPoupanca);
-            when(transacaoRepository.existsByContaIdAndTipoTransacaoAndDataHoraBetween(eq(contaId), any(), any(), any()))
-                    .thenReturn(true);
+            when(contaPoupanca.getId()).thenReturn(contaId);
+            when(contaPoupanca.getSituacaoConta()).thenReturn(SituacaoConta.ATIVA);
+            when(contaService.buscarContaPorId(contaId)).thenReturn(contaPoupanca);
+            when(transacaoRepository.existsByContaIdAndTipoTransacaoAndDataHoraBetween(eq(contaId), any(), any(), any())).thenReturn(true);
             assertThrows(RendimentoJaAplicadoException.class, () -> transacaoService.aplicarRendimento(contaId));
             verify(transacaoRepository, never()).save(any());
         }
@@ -126,19 +99,12 @@ public class TransacaoServiceTest {
         void deveAplicarRendimentoComSucesso() {
             Long contaId = 1L;
             ContaPoupanca contaPoupanca = mock(ContaPoupanca.class);
-            when(contaPoupanca.getId())
-                    .thenReturn(contaId);
-            when(contaPoupanca.getSituacaoConta())
-                    .thenReturn(SituacaoConta.ATIVA);
-            when(contaPoupanca.podeReceberRendimento())
-                    .thenReturn(true);
-            when(contaPoupanca.calcularRendimento())
-                    .thenReturn(new BigDecimal("15.50"));
-            when(contaService.buscarContaPorId(contaId))
-                    .thenReturn(contaPoupanca);
-            when(transacaoRepository.existsByContaIdAndTipoTransacaoAndDataHoraBetween(
-                    eq(contaId), any(), any(), any()
-            )).thenReturn(false);
+            when(contaPoupanca.getId()).thenReturn(contaId);
+            when(contaPoupanca.getSituacaoConta()).thenReturn(SituacaoConta.ATIVA);
+            when(contaPoupanca.podeReceberRendimento()).thenReturn(true);
+            when(contaPoupanca.calcularRendimento()).thenReturn(new BigDecimal("15.50"));
+            when(contaService.buscarContaPorId(contaId)).thenReturn(contaPoupanca);
+            when(transacaoRepository.existsByContaIdAndTipoTransacaoAndDataHoraBetween(eq(contaId), any(), any(), any())).thenReturn(false);
             transacaoService.aplicarRendimento(contaId);
             verify(contaPoupanca).creditar(new BigDecimal("15.50"));
             verify(transacaoRepository, times(1)).save(any());

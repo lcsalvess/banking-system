6cf46e5fe44eb9b5ececfd90f001fee3901f7e78
Lucas Alves <lucas0asm@outlook.com>
2026-09-14 20:16:27 -0300

    refactor(exception): return conflict for repeated daily yield

--- src/main/java/com/lucas/sistemabancario/exception/transaction/YieldAlreadyAppliedException.java
+++ src/main/java/com/lucas/sistemabancario/exception/transaction/YieldAlreadyAppliedException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class YieldAlreadyAppliedException extends BusinessException {
     public YieldAlreadyAppliedException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
+        super(message, HttpStatus.CONFLICT);
     }
 }
